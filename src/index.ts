2146b09b0d6042b40b78a2c44cd5af7e0267fe53
Julien Biezemans <jb@jbpros.com>
2020-12-26 00:59:07 +0100

    Start bringing back the formatter

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
-import { Formatter } from '@cucumber/cucumber'
+import { Formatter, Status } from '@cucumber/cucumber'
 import { IFormatterOptions } from '@cucumber/cucumber/lib/formatter'
+import {
+  formatIssue,
+  formatSummary,
+} from '@cucumber/cucumber/lib/formatter/helpers'
+import {
+  getGherkinScenarioMap,
+  getGherkinStepMap,
+} from '@cucumber/cucumber/lib/formatter/helpers/gherkin_document_parser'
+import { durationBetweenTimestamps } from '@cucumber/cucumber/lib/time'
+import { getPickleStepMap } from '@cucumber/cucumber/lib/formatter/helpers/pickle_parser'
+import { messages } from '@cucumber/messages'
+import * as colors from 'colors'
+import { cross, tick } from 'figures'
+import { EOL as n } from 'os'
 
-// const marks = {
-//   ambiguous: cross,
-//   failed: cross,
-//   passed: tick,
-//   pending: '?',
-//   skipped: '-',
-//   undefined: '?',
-// }
+const marks = {
+  [Status.AMBIGUOUS]: cross,
+  [Status.FAILED]: cross,
+  [Status.PASSED]: tick,
+  [Status.PENDING]: '?',
+  [Status.SKIPPED]: '-',
+  [Status.UNDEFINED]: '?',
+}
 
 /** @see https://github.com/cli-table/cli-table3#custom-styles */
 // const table = {
@@ -33,120 +47,164 @@ import { IFormatterOptions } from '@cucumber/cucumber/lib/formatter'
 // }
 
 export default class PrettyFormatter extends Formatter {
+  // TODO: review this:
   private uri?: string = undefined
+  private colorsEnabled = false
+  private testRunStartedTimestamp: messages.ITimestamp = {
+    seconds: 0,
+    nanos: 0,
+  }
 
   constructor(options: IFormatterOptions) {
     super(options)
+    this.parseEnvelope = this.parseEnvelope.bind(this)
 
-    //   options.eventBroadcaster.on('test-case-started', (event) => {
-    //     const {
-    //       gherkinDocument,
-    //       pickle,
-    //     } = options.eventDataCollector.getTestCaseAttempt(event)
+    options.eventBroadcaster.on('envelope', this.parseEnvelope)
+  }
 
-    //     if (this.uri !== event.sourceLocation.uri) {
-    //       const { feature } = gherkinDocument
+  private parseEnvelope(envelope: messages.Envelope) {
+    if (envelope.testRunStarted) this.onTestRunStarted(envelope.testRunStarted)
+    if (envelope.testCaseStarted)
+      this.onTestCaseStarted(envelope.testCaseStarted)
+    if (envelope.testStepStarted)
+      this.onTestStepStarted(envelope.testStepStarted)
+    if (envelope.testStepFinished)
+      this.onTestStepFinished(envelope.testStepFinished)
+    if (envelope.testCaseFinished)
+      this.onTestCaseFinished(envelope.testCaseFinished)
+    if (envelope.testRunFinished)
+      this.onTestRunFinished(envelope.testRunFinished)
+  }
 
-    //       if (this.uri) this.logn()
+  private onTestRunStarted(testRunStarted: messages.ITestRunStarted) {
+    this.testRunStartedTimestamp =
+      typeof testRunStarted.timestamp === 'number'
+        ? testRunStarted.timestamp
+        : { seconds: 0, nanos: 0 }
+  }
 
-    //       const tags = feature.tags.map((tag: any) => tag.name).join(' ')
-    //       if (tags) this.logn(options.colorFns.tag(tags))
+  private onTestCaseStarted(testCaseStarted: messages.ITestCaseStarted) {
+    const {
+      gherkinDocument,
+      pickle,
+    } = this.eventDataCollector.getTestCaseAttempt(testCaseStarted.id || '')
+    const { feature } = gherkinDocument
+    if (this.uri !== gherkinDocument.uri && feature) {
+      if (this.uri) this.logn()
+      this.renderFeatureHead(feature)
+      // TODO: what do we do when there is no URI?
+      this.uri = gherkinDocument.uri || ''
+    }
 
-    //       this.logn(
-    //         `${this.color(feature.keyword, 'magenta', 'bold')}: ${feature.name}`
-    //       )
+    this.renderScenarioHead(gherkinDocument, pickle)
+  }
 
-    //       if (feature.description && this.descriptionEnabled)
-    //         this.logn(`${n}${feature.description}`)
+  private onTestStepStarted(testStepStarted: messages.ITestStepStarted) {
+    const {
+      gherkinDocument,
+      pickle,
+      testCase,
+    } = this.eventDataCollector.getTestCaseAttempt(
+      testStepStarted.testCaseStartedId || ''
+    )
 
-    //       this.uri = event.sourceLocation.uri
-    //     }
+    const pickleStepMap = getPickleStepMap(pickle)
+    const gherkinStepMap = getGherkinStepMap(gherkinDocument)
+    const testStep = (testCase.testSteps || []).find(
+      (item) => item.id === testStepStarted.testStepId
+    )
 
-    //     this.logn()
+    if (testStep && testStep.pickleStepId !== '') {
+      const pickleStep = pickleStepMap[testStep.pickleStepId]
+      const astNodeId = pickleStep.astNodeIds[0]
+      const gherkinStep = gherkinStepMap[astNodeId]
+      this.logn(
+        `${this.color(gherkinStep.keyword.trim(), 'bold')} ${gherkinStep.text}`,
+        4
+      )
+    }
+  }
 
-    //     const tags = pickle.tags.map((tag: any) => tag.name).join(' ')
-    //     if (tags) this.logn(options.colorFns.tag(tags), 2)
+  private onTestStepFinished(testStepFinished: messages.ITestStepFinished) {
+    const { message, status } = testStepFinished.testStepResult || {}
 
-    //     const line = Math.min(
-    //       ...pickle.locations.map((location: any) => location.line)
-    //     )
-    //     const { keyword } = gherkinDocument.feature.children.find(
-    //       (child: any) => child.location.line === line
-    //     )
+    if (status && status !== Status.PASSED) {
+      this.logn(
+        this.colorFns.forStatus(status)(
+          ` ${marks[status]} ${Status[status].toLowerCase()}`
+        ),
+        4
+      )
 
-    //     this.logn(`${this.color(keyword, 'magenta', 'bold')}: ${pickle.name}`, 2)
-    //   })
+      if (message) {
+        const testCaseAttempt = this.eventDataCollector.getTestCaseAttempt(
+          testStepFinished.testCaseStartedId || ''
+        )
+        const error = formatIssue({
+          colorFns: this.colorFns,
+          cwd: this.cwd,
+          number: 9999, // TODO: get this number
+          snippetBuilder: this.snippetBuilder,
+          supportCodeLibrary: this.supportCodeLibrary,
+          testCaseAttempt,
+        })
+        this.logn(error, 6)
+      }
+    }
+  }
 
-    //   options.eventBroadcaster.on('test-step-started', (event) => {
-    //     const testCaseAttempt = options.eventDataCollector.getTestCaseAttempt(
-    //       event.testCase
-    //     )
-    //     testCaseAttempt.stepResults = testCaseAttempt.testCase.steps.map(
-    //       () => ({})
-    //     )
+  private onTestCaseFinished(_testCaseFinished: messages.ITestCaseFinished) {
+    // this.logn()
+  }
 
-    //     const testStep = formatterHelpers.parseTestCaseAttempt({
-    //       testCaseAttempt,
-    //     }).testSteps[event.index]
-    //     if (!testStep.sourceLocation) return // hook
+  private onTestRunFinished(testRunFinished: messages.ITestRunFinished) {
+    const testRunDuration = durationBetweenTimestamps(
+      this.testRunStartedTimestamp,
+      testRunFinished.timestamp || { nanos: 0, seconds: 0 }
+    )
+    if (this.uri) this.logn()
+    this.log(
+      formatSummary({
+        colorFns: this.colorFns,
+        testCaseAttempts: this.eventDataCollector.getTestCaseAttempts(),
+        testRunDuration,
+      })
+    )
+  }
 
-    //     this.logn(
-    //       `${this.color(testStep.keyword.trim(), 'bold')} ${testStep.text}`,
-    //       4
-    //     )
+  private renderFeatureHead(feature: messages.GherkinDocument.IFeature) {
+    const tags = (feature?.tags || []).map((tag) => tag.name).join(' ')
+    if (tags) this.logn(this.colorFns.tag(tags))
+    this.logn(
+      `${this.color(feature.keyword, 'magenta', 'bold')}: ${feature.name}`
+    )
 
-    //     testStep.arguments.forEach((argument: any) => {
-    //       if (argument.content) {
-    //         this.logn(`"""${n}${argument.content}${n}"""`, 6)
-    //       }
+    if (feature.description) this.logn(`${n}${feature.description}`)
+  }
 
-    //       if (argument.rows) {
-    //         const datatable = new Table(table)
-    //         datatable.push(...argument.rows)
-    //         this.logn(datatable.toString(), 6)
-    //       }
-    //     })
-    //   })
+  private renderScenarioHead(
+    gherkinDocument: messages.IGherkinDocument,
+    pickle: messages.IPickle
+  ) {
+    this.logn()
+    const tags = (pickle.tags || []).map((tag) => tag.name).join(' ')
+    if (tags) this.logn(this.colorFns.tag(tags), 2)
+    const gherkinScenarioMap = getGherkinScenarioMap(gherkinDocument)
+    if (!pickle.astNodeIds) throw new Error('Pickle AST nodes missing')
+    const keyword = gherkinScenarioMap[pickle.astNodeIds[0]].keyword
+    this.logn(`${this.color(keyword, 'magenta', 'bold')}: ${pickle.name}`, 2)
+  }
 
-    //   options.eventBroadcaster.on('test-step-finished', (event) => {
-    //     const {
-    //       result: { status, exception },
-    //     } = event
+  private logn(value = '', indent = 0) {
+    let text = value.toString()
+    if (indent > 0) text = text.replace(/^/gm, ' '.repeat(indent))
+    this.log(`${text}${n}`)
+  }
 
-    //     if (status !== 'passed') {
-    //       this.logn(
-    //         options.colorFns[status](
-    //           `${marks[status as keyof typeof marks]} ${status}`
-    //         ),
-    //         4
-    //       )
-    //     }
-
-    //     if (exception) {
-    //       const error = formatterHelpers.formatError(exception, options.colorFns)
-    //       this.logn(error, 6)
-    //     }
-    //   })
-
-    //   options.eventBroadcaster.on('test-run-finished', (event) => {
-    //     const noptions = Object.create(options, {
-    //       eventBroadcaster: { value: { on: () => {} } },
-    //     })
-    //     const formatter = new SummaryFormatter(noptions)
-    //     if (this.uri) this.logn()
-    //     ;(formatter as any).logSummary(event)
-    //   })
-    // }
-
-    // color(value: any, ...color: any[]) {
-    //   return this.colorsEnabled
-    //     ? color.reduce((v, c) => v[c], colors)(value)
-    //     : value
-    // }
-
-    // logn(value = '', indent = 0) {
-    //   let text = value.toString()
-    //   if (indent > 0) text = text.replace(/^/gm, ' '.repeat(indent))
-    //   this.log(`${text}${n}`)
+  private color(value: any, ...color: any[]) {
+    // TODO: fix this
+    return this.colorsEnabled
+      ? color.reduce((v, c) => v[c], colors)(value)
+      : value
   }
 }
