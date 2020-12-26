6a68b520c038f924c11ecbc8d73061b39cb070a1
Julien Biezemans <jb@jbpros.com>
2020-12-26 23:48:19 +0100

    Refactor world test

--- test/world.ts
+++ test/world.ts
@@ -1,13 +1,9 @@
 import 'should'
 
-import { exec } from './exec'
-
-const args = ['--format', '.']
+import { run } from './exec'
 
 describe('World', () => {
   it('should not throw colors error', () => {
-    exec('test/features/world.feature', ...args).should.containEql(
-      '\n\u001b[34mWORLD\u001b[39m\n'
-    )
+    run('world.feature').should.containEql('\n\u001b[34mWORLD\u001b[39m\n')
   })
 })
