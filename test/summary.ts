557eb92b72a7ffbcf5cdd3e68397950f508197a7
Julien Biezemans <jb@jbpros.com>
2020-12-26 18:41:23 +0100

    Fix summary test

--- test/summary.ts
+++ test/summary.ts
@@ -4,9 +4,12 @@ import { args, exec } from './exec'
 
 describe('Summary', () => {
   it('should log empty summary', () => {
-    exec('features', '--tags', '@empty', ...args).should.equal(
-      '0 scenarios\n' + '0 steps\n' + '0m00.000s\n'
-    )
+    exec(
+      'test/features/feature.feature',
+      '--tags',
+      '@empty',
+      ...args
+    ).should.equal('0 scenarios\n' + '0 steps\n' + '0m00.000s\n')
   })
 
   it('should log new line', () => {
